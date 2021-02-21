2fda6f4c0d7bd56b54a058f7e7595e4c42738b05
Michael Gale <m.gale@warwick.ac.uk>
2021-02-21 17:44:44 +0000

    Update for 20/21

--- src/Lecture.hs
+++ src/Lecture.hs
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------
 -- Functional Programming (CS141)                                             --
--- Lecture: Applicative functors (cont.)                                      --
+-- Lecture: Applicative functors                                              --
 --------------------------------------------------------------------------------
 
 module Lecture where
@@ -14,20 +14,6 @@ infixl 4 <*>
 class Functor f => Applicative f where
     pure :: a -> f a
     (<*>) :: f (a -> b) -> f a -> f b
-
-instance Applicative [] where
-    pure x = [x]
-
-    fs <*> xs = [f x | f <- fs, x <- xs]
-
-listEx0 :: (a -> b) -> (a -> b) -> a -> a -> a -> [b]
-listEx0 f g x y z = [f,g] <*> [x,y,z]
-
-listEx1 :: (a -> b -> c) -> a -> a -> b -> b -> b -> [c]
-listEx1 g x y a b c = g <$> [x,y] <*> [a,b,c]
-
-listEx2 :: Num a => a -> a -> a -> a -> a -> [a]
-listEx2 u v a b c = (((\x y z -> x+y+z) <$> [u,v]) <*> [a,b,c]) <*> [1,2]
 
 instance Applicative Maybe where
     pure = Just
@@ -108,5 +94,18 @@ ex4 = fmap (+5) $ MkWriter (4,[])
 
 writeLog :: String -> Writer [String] ()
 writeLog msg = MkWriter ((), [msg])
+
+instance Monoid w => Applicative (Writer w) where
+    pure x = MkWriter (x, mempty)
+
+    MkWriter (f,o1) <*> MkWriter (x,o2) = MkWriter (f x, o1 <> o2)
+
+comp :: Expr -> Writer [String] Program
+comp (Val n)    = writeLog "compiling a value" *> pure [PUSH n]
+comp (Plus l r) = writeLog "compiling a plus" *>
+    ((\p p' -> p ++ p' ++ [ADD]) <$> comp l <*> comp r)
+
+compEx :: Writer [String] Program
+compEx = comp (Plus (Val 4) (Val 8))
 
 --------------------------------------------------------------------------------
